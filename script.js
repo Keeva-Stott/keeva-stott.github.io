85003461f388bbbecf17d3daf8290cc1794e33ac
Keeva-Stott <stottkeeva@gmail.com>
2025-03-08 16:12:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    strings: ["Hands-on Experience" , "Sports" , "Start ups"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
